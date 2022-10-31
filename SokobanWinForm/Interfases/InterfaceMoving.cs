4b0bfcfdaf5c8c05c1a3d62e4db2ef4493e3a354
Dedulko Nikita <36064675+DedulkoN@users.noreply.github.com>
2022-10-31 11:18:10 +0300

    InterfaceMoving

---
+++ SokobanWinForm/Interfases/InterfaceMoving.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokobanWinForm.Interfases
+{
+    /// <summary>
+    /// Интерфейс движения
+    /// </summary>
+    internal interface InterfaceMoving
+    {
+        /// <summary>
+        /// вверх
+        /// </summary>
+        /// <returns>истина, если удачно</returns>
+        bool moveUp();
+        /// <summary>
+        /// вниз
+        /// </summary>
+        /// <returns>истина, если удачно</returns>
+        bool moveDown();
+        /// <summary>
+        /// налево
+        /// </summary>
+        /// <returns>истина, если удачно</returns>
+        bool moveLeft();
+        /// <summary>
+        /// направо
+        /// </summary>
+        /// <returns>истина, если удачно</returns>
+        bool moveRight();
+
+    }
+}
