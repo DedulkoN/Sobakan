7d8830173c30fec0b0c0bf557af0fa9ec1a634dc
Dedulko Nikita <36064675+DedulkoN@users.noreply.github.com>
2022-10-31 11:11:08 +0300

    Add ClassLevelMatrix

---
+++ SokobanWinForm/Classes/ClassLevelMatrix.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokobanWinForm.Classes
+{
+    /// <summary>
+    /// Значения ячеек матрицы уровня
+    /// </summary>
+    public enum MatrixValue 
+    { 
+        /// <summary>
+        /// Игрок
+        /// </summary>
+        p, 
+        /// <summary>
+        /// Ящик
+        /// </summary>
+        b, 
+        /// <summary>
+        /// Стена
+        /// </summary>
+        w, 
+        /// <summary>
+        /// Целевая точка
+        /// </summary>
+        t,
+        /// <summary>
+        /// Ящик на целевой точке
+        /// </summary>
+        bt, 
+        /// <summary>
+        /// Пустая клетка
+        /// </summary>
+        z 
+    }
+
+    /// <summary>
+    /// матрица уровня
+    /// </summary>
+    public class ClassLevelMatrix
+    {
+        /// <summary>
+        /// сама матрица уровня
+        /// </summary>
+        public MatrixValue[,] LevelMatrix;
+
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        /// <param name="rows">кол-во строк</param>
+        /// <param name="columns">кол-во столбцов</param>
+        public ClassLevelMatrix(int rows, int columns )
+        {
+            LevelMatrix = new MatrixValue[rows, columns];
+        }
+
+
+    }
+}
